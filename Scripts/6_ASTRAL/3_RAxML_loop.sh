74b4220b303bc691c85e2d6a6bc3c11ce6a07758
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2025-01-13 20:41:19 +0100

    Create 3_RAxML_loop.sh

---
+++ Scripts/6_ASTRAL/3_RAxML_loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=2-12:00:00
+#SBATCH --partition=cpu_natbio
+#SBATCH --output=output_%j.txt
+#SBATCH --error=error_output_%j.txt
+#SBATCH --job-name=ASTRAL_RAxML
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=
+
+## Bash Script to loop RAxML script
+
+cd /data1/s2321041/Analyses/4.ASTRAL/4.3_RAxML/
+
+for file in *.phy
+do
+/data1/s2321041/Analyses/4.ASTRAL/4.3_RAxML/standard-RAxML-master/raxmlHPC-PTHREADS-SSE3 -T 2 -f a -x 124473 -p 124473 -N 100 -m ASC_GTRGAMMA --asc-corr=lewis -O -n ${file%.phy}.tre -s "$file" -w "/data1/s2321041/Analyses/4.ASTRAL/4.3_RAxML"
+done
